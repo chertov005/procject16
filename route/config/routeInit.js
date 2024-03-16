ddb4e912a42209cfa9061708b6a24f0c4c369c24
Dani Chertov <chertov005@gmail.com>
2024-03-17 00:41:30 +0200

    333

--- route/config/routeInit.js
+++ route/config/routeInit.js
@@ -1,9 +1,11 @@
 
 const usersR = require('../route_list/usersRoute')
 const foodsR = require('../route_list/foodsRoute')
+const indexR = require('../route_list/indexR')
 
 exports.routeInit = (_app) => {
 
+    _app.use('/' , indexR);
     _app.use('/users' , usersR);
     _app.use('/foods' ,foodsR);
 
