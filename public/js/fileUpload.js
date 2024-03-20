3af530412943fc8a6cbab33f7e460db8cad90f0c
Dani Chertov <chertov005@gmail.com>
2024-03-20 05:29:58 +0200

    2

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -1,4 +1,57 @@
 
+// window.addEventListener('load' , async() => {
+
+//     declareViewEvent()
+
+// });
+
+
+
+
+// const declareViewEvent = async() => {
+
+//     let id_form = document.querySelector('#id_form');
+//     id_form.addEventListener('submit' , (e) => {
+
+//         e.preventDefault()
+
+//         let inp_file = document.querySelector('#id_file').files[0] 
+
+//         let dataMyForm = new FormData();
+
+//         dataMyForm.append('file' ,inp_file)
+
+//         doApiUpload(dataMyForm)
+
+
+//     });
+    
+
+
+// };
+
+
+
+// const doApiUpload = async(_file) => {
+
+//     let url = `http://127.0.0.1:3002/upload` ;
+
+//     let resp = await fetch(url ,{
+//         method:'POST',
+//         body:_file
+//     })
+//     let data = await resp.json()
+
+//     console.log(data)
+
+
+
+// };
+
+
+
+
+
 window.addEventListener('load' , async() => {
 
     declareViewEvent()
@@ -8,42 +61,42 @@ window.addEventListener('load' , async() => {
 
 
 
-const declareViewEvent = async() => {
+const doApiUpload = async(inp_file) => {
 
-    let id_form = document.querySelector('#id_form');
-    id_form.addEventListener('submit' , (e) => {
-
-        e.preventDefault()
-
-        let inp_file = document.querySelector('#id_file').files[0] 
-
-        let dataMyForm = new FormData();
-
-        dataMyForm.append('file' ,inp_file)
-
-        doApiUpload(dataMyForm)
-
-
-    });
-    
-
-
-};
-
-
-
-const doApiUpload = async(_file) => {
-
-    let url = `http://127.0.0.1:3002/upload` ;
-
-    let resp = await fetch(url ,{
+    let url = 'http://127.0.0.1:3002/upload' 
+    let resp = await fetch(url, {
         method:'POST',
-        body:_file
-    })
+        body:inp_file
+    }) 
     let data = await resp.json()
 
     console.log(data)
 
+    
+};
+
+
+
+
+const declareViewEvent = () => {
+    
+    let id_form = document.querySelector('#id_form') ;
+    id_form.addEventListener('submit' , async(e) => {
+        
+        e.preventDefault() ;
+        
+        let inp_file = document.querySelector('#id_file').files[0] ;
+        
+        let formMyData = new FormData();
+
+        formMyData.append('file' ,inp_file)
+
+
+    
+        doApiUpload(formMyData)
+        
+        
+    })
 
 
 };
